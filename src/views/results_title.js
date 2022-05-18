1d6db8d3f95442a51c92480f148363b1b13a4792
simonalai <simona_lai@icloud.com>
2022-05-18 19:07:36 +0200

    finito bottone aggiungi ai preferiti

--- src/views/results_title.js
+++ src/views/results_title.js
@@ -52,7 +52,7 @@ function aggiungiPreferiti(id_utente, title){
 function aggiornaButton(){
     var e= document.getElementById("heart-button");
     if (e.innerHTML=='Aggiungi ai preferiti <i class="fa fa-heart-o"></i>'){
-        e.innerHTML="Aggiunto! <i class=\"fa fa-heart\"></i>";
+        e.innerHTML='Aggiunto! <i class="fa fa-heart"></i>';
     }
     else{
         e.innerHTML='Aggiungi ai preferiti <i class="fa fa-heart-o"></i>'
