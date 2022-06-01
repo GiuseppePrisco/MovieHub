557cf6d9bb350fd65983e187987f83ebdd08907e
ElenaBianchini <elenabianchini_@hotmail.it>
2022-06-01 14:57:06 +0200

    aggiunta del test

---
+++ src/test.js
@@ -0,0 +1,44 @@
+var should = require('chai').should();
+var assert = require('chai').assert
+var https = require('https');
+const axios = require('axios');
+
+const agent = new https.Agent({  
+  rejectUnauthorized: false
+});
+
+describe("Test if the website is up and running", function() {
+    it("should return 200", function(done) {
+        axios.get('https://localhost:3000/', {httpsAgent: agent}).then(function(response) {
+	       response.status.should.equal(200);
+		   done();
+	}).catch(function(error) {
+		   done(error);
+	});
+  });
+});
+
+
+describe("Test if REST API for searching a film", function() {
+    it("should return 200", function(done) {
+        axios.post('https://localhost:3000/', {
+            "search": "The Great Gatsby"
+		}, {httpsAgent: agent}).then(function(response) {
+			response.status.should.equal(200);
+			done();
+		}).catch(function(error) {
+			done(error);
+		});
+	})
+});
+
+describe("Test if REST API for getting the top ten movies on Netflix", function() {
+    it("should return 200", function(done) {
+        axios.get('https://localhost:3000/topMovie', {httpsAgent: agent}).then(function(response) {
+			response.status.should.equal(200);
+		    done()
+		}).catch(function(error) {
+			done(error);
+		});
+	});
+});
